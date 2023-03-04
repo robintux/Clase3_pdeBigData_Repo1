b240e715dfe00bd66ef37bca18b888d6a76fc4d5
AzamudioWindows <abraham.zamudio@gmail.com>
2023-03-04 11:52:15 -0500

    Agregamos algunas lineas luego de configurar nuestro entorno de trabajo (Anaconda Prompt)

--- Script01.py
+++ Script01.py
@@ -1,8 +1,17 @@
+# %% Cargamos modulos y datos 
+
 import numpy as np
 import pandas as pd 
-import matlplotlib.pyplot as plt 
-import sympy
+import matplotlib.pyplot as plt 
+# import sympy
 
+# Datos 
+from sklearn.datasets import load_digits
+digits = load_digits()
 
 # Agregamos un conjuntoo de datos a analizar para realizar una regresion
 # logistica multiclase
+
+
+
+
